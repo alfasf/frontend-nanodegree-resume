994589bc2faa219441b3f7653251644cd7326584
alfasf <diazalfredo83@gmail.com>
2017-11-12 21:20:37 -0800

    appending header name

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+/*
+ $("#main").append("Alfredo Diaz-Swain");
+ var awesomeThoughts = "My name is Alfredo and I am awesome!";
+ var funThoughts = awesomeThoughts.replace("awesome", "FUN");
+
+ $('#main').append(funThoughts); */
+
+var formattedName= HTMLheaderName.replace("%data%", "Alfredo Diaz-Swain");
+var formattedRole = HTMLheaderRole.replace("%data%", "Front-End Web Developer");
+
+$('#header').prepend(formattedRole);
+$("#header").prepend(formattedName);
+ 
