935bd9f63a2c6733144aa935301b20cc7796dc0e
alfasf <diazalfredo83@gmail.com>
2017-11-15 06:59:17 -0800

    continue filling resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -35,24 +35,30 @@ $("#header").append(formattedRole);
 
 var skills= bio.skills;
 var formattedSkills= HTMLskills.replace("%data%", skills);
+$("#header").append(HTMLskillsStart);
+$("#skills").append(formattedSkills);
 
 var contacMobile= bio.contacts.mobile;
 var formattedMobile= HTMLmobile.replace("%data%", contacMobile);
 $("#topContacts").prepend(formattedMobile);
 
-var contactLocation= bio.contacts.location; 
-var formattedLocation= HTMLlocation.replace("%data%", contactLocation);
-
 var contactEmail= bio.contacts.email; 
 var formattedEmail= HTMLemail.replace("%data%", contactEmail);
+$("#topContacts").prepend(formattedEmail);
+
+var contactLocation= bio.contacts.location; 
+var formattedLocation= HTMLlocation.replace("%data%", contactLocation);
+$("#topContacts").append(contactLocation);
 
 var bioPic= bio.bioPic;
 var picture= HTMLbioPic.replace("%data%", bioPic);
+$("#header").prepend(picture);
 
 var message= bio.welcomeMessage;
 var welcome= HTMLwelcomeMsg.replace("%data%", message);
+$("#header").prepend(welcome)
 
 
-$('#header').prepend(bio);
+//$('#header').prepend(bio);
 //$("#header").prepend(formattedName);
  
